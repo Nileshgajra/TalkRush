684d6981b02adf2fd119d93bb3ddce33f8cbe97c
Nileshgajra <83764885+Nileshgajra@users.noreply.github.com>
2026-06-05 18:03:30 +0530

    Added AdMob initialization in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import mobileAds from 'react-native-google-mobile-ads';
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    mobileAds().initialize();
+  }, []);
+
   return (
     <Stack
       screenOptions={{
